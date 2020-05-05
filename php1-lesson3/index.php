b99113a7375da29cf27f35f7387b445bec4c91eb
ildar <islamov.ildar@gmail.com>
2020-05-05 11:26:59 +0500

    Lesson 3, task 1, 2, 3 done

--- php1-lesson3/index.php
+++ php1-lesson3/index.php
@@ -1,4 +1,6 @@
 <?php
+header('content-type: text/html; charset=utf-8');
+
 echo "<h2>Задание 1</h2>";
 
 $x = 1;
@@ -7,7 +9,7 @@ while($x<101){
 
 if($x % 3 == 0){
 
-    echo $x;
+    echo $x . " ";
 
 }
 
@@ -17,21 +19,100 @@ $x++;
 
 echo "<h2>Задание 2</h2>";
 $x = 0;
-echo $x . " - ноль ";
+
 do{
+
+if($x == 0){
+
+echo $x . " - ноль; ";
+
+}
 
 ++$x;
 
 if($x % 2 !== 0) {
 
-echo $x . " -нечетное число ";
+echo $x . " -нечетное число; ";
 
 } else {
 
-echo $x . " -четное число ";
+echo $x . " -четное число; ";
 
 }
 
 } while ($x < 11)
+;
+
+echo "<h2>Задание 3</h2>";
+
+$cityRegionArray = [
+
+'MoscowRegion' => ['Moscow', 'Zelenograd', 'Klin'],
+'LeningradRegion' => ['St.Petersburg', 'Vsevolozhsk', 'Pavlovsk', 'Kronstadt'],
+'RyazanRegion' => ['Ryazan', 'Kasimov', 'Korablino', 'Novomichurinsk']
+
+];
+
+foreach($cityRegionArray as $region => $cityArr) {
+
+echo "<h4>$region:</h4>";
+
+    for ($i = 0; $i < count($cityArr); $i++) {
+
+        echo $cityArr[$i].  ' <br> ';
+
+        if($i == count($cityArr) - 1) {
+
+            echo "<br>";
+
+        }
+
+    }
+
+}
+
+echo "<h2>Задание 4</h2>";
+
+$letterArr = [
+'а' => 'a',
+'б' => 'b',
+'в' => 'v',
+'г' => 'g',
+'д' => 'd',
+'е' => 'e',
+'ё' => 'e',
+'ж' => 'zh',
+'з' => 'z',
+'и' => 'i',
+'й' => 'y',
+'к' => 'k',
+'л' => 'l',
+'м' => 'm',
+'н' => 'n',
+'о' => 'o',
+'п' => 'p',
+'р' => 'r',
+'с' => 's',
+'т' => 't',
+'у' => 'u',
+'ф' => 'f',
+'х' => 'kh',
+'ц' => 'ts',
+'ч' => 'ch',
+'ш' => 'sh',
+'щ' => 'sh',
+'ь' => '',
+'ы' => 'y',
+'ъ' => '',
+'э' => 'e',
+'ю' => 'yu',
+'я' => 'ya',
+];
+
+$myWord = "Слово";
+
+$wordArr = str_split($myWord);
+
+var_dump ($wordArr);
 
 ?>
